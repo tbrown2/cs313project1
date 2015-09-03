19520b296be7aa5cae7a7dd9d07ff08b4e571993
Thomas Brown <tbrown2@luc.edu>
2015-09-03 13:22:27 -0500

    changed year

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -2,9 +2,12 @@ package hw;
 
 public class Main {
 
-	public static void main(String[] args) {
-		final HelloWorld instance = new HelloWorld();
+	public static void main(String[] args)
+	{
+		final HelloWorld instance;
+		instance = new HelloWorld();
 		System.out.println(instance.getMessage());
 		System.out.println(instance.getYear());
+
 	}
 }
